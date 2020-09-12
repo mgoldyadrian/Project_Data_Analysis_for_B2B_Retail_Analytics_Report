9c7124f3ba53598d23d608212def207eb7fc4cd2
Muhammad Goldy Adrian <61867283+mgoldyadrian@users.noreply.github.com>
2020-09-12 12:25:11 +0700

    Update customer-growth.sql

--- customer-growth.sql
+++ customer-growth.sql
@@ -1,14 +1,14 @@
-#Is the number of customers growing?
+# Is the number of customers growing?
 
-SELECT quarter, COUNT(customerID) as total_customers
-FROM (SELECT customerID, createDate, '1' as quarter
-FROM customer WHERE createDate between '2004-01-01' and '2004-03-31'
+SELECT quarter, COUNT(customerID) AS total_customers
+FROM (SELECT customerID, createDate, '1' AS quarter
+FROM customer WHERE createDate BETWEEN '2004-01-01' AND '2004-03-31'
 UNION
-SELECT customerID, createDate, '2' as quarter
-FROM customer WHERE createDate between '2004-04-01' and '2004-06-31') as tabel_b
+SELECT customerID, createDate, '2' AS quarter
+FROM customer WHERE createDate BETWEEN '2004-04-01' AND '2004-06-31') AS tabel_b
 GROUP BY quarter;
 
-#output
+# Output
 +---------+-----------------+
 | quarter | total_customers |
 +---------+-----------------+
@@ -16,18 +16,18 @@ GROUP BY quarter;
 | 2       |              35 |
 +---------+-----------------+
 
-#How many of these customers have made transactions?
+# How many of these customers have made transactions?
 
-SELECT quarter, COUNT(DISTINCT customerID) as total_customers
-FROM (SELECT customerID, createDate, quarter(createDate) as quarter
+SELECT quarter, COUNT(DISTINCT customerID) AS total_customers
+FROM (SELECT customerID, createDate, quarter(createDate) AS quarter
 FROM customer
-WHERE createDate BETWEEN '2004-01-01' AND '2004-07-01') as tabel_b
+WHERE createDate BETWEEN '2004-01-01' AND '2004-07-01') AS tabel_b
 WHERE customerID IN (SELECT DISTINCT customerID FROM orders_1
 UNION
 SELECT DISTINCT customerID FROM orders_2)
 GROUP BY quarter;
 
-#output
+# Output
 +---------+-----------------+
 | quarter | total_customers |
 +---------+-----------------+
@@ -35,7 +35,7 @@ GROUP BY quarter;
 |       2 |              19 |
 +---------+-----------------+ 
 
-#Which product categories are the most ordered by customers in Quarter-2?
+# Which product categories are the most ordered by customers in Quarter-2?
 
 SELECT * FROM (SELECT categoryID, COUNT(DISTINCT orderNumber) AS total_order, SUM(quantity) AS total_penjualan
 FROM ( SELECT productCode, orderNumber, quantity, status, LEFT(productCode,3) AS categoryID
@@ -43,7 +43,7 @@ FROM orders_2
 WHERE status = "Shipped") tabel_c
 GROUP BY categoryID ) a ORDER BY total_order DESC
 
-#output
+# Output
 +------------+-------------+-----------------+
 | categoryID | total_order | total_penjualan |
 +------------+-------------+-----------------+
@@ -57,14 +57,14 @@ GROUP BY categoryID ) a ORDER BY total_order DESC
 | S72        |           2 |              61 |
 +------------+-------------+-----------------+ 
 
-#How many customers remain active transactions after the first transaction?
+# How many customers remain active transactions after the first transaction?
 
-#Menghitung total unik customers yang transaksi di quarter_1
-SELECT COUNT(DISTINCT customerID) as total_customers FROM orders_1;
-#output = 25
-SELECT 1 as quarter, (COUNT(DISTINCT customerID)*100)/25 as Q2 FROM orders_1 WHERE customerID IN (SELECT DISTINCT customerID FROM orders_2);
+# Menghitung total unik customers yang transaksi di quarter_1
+SELECT COUNT(DISTINCT customerID) AS total_customers FROM orders_1;
+# Output = 25
+SELECT 1 AS quarter, (COUNT(DISTINCT customerID)*100)/25 AS Q2 FROM orders_1 WHERE customerID IN (SELECT DISTINCT customerID FROM orders_2);
 
-#output
+# Output
 +-----------------+
 | total_customers |
 +-----------------+
